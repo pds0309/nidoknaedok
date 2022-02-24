1f4b0fe4bc862683085d068326bbcfe459d2745b
pds0309 <ehd0309@gmail.com>
2022-02-25 01:17:27 +0900

    Feat: REST 전역 응답 객체 추가

---
+++ src/com/dto/common/ResponseDTO.java
@@ -0,0 +1,22 @@
+package com.dto.common;
+
+import com.errors.ErrorResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONObject;
+/**
+ * REST 응답을 위한 객체이다.
+ */
+public class ResponseDTO<T> {
+
+    public static <T> JSONObject toJson(int status, T data) throws JsonProcessingException {
+        return new JSONObject()
+                .put("data", new JSONObject(new ObjectMapper().writeValueAsString(data)))
+                .put("status", status);
+    }
+
+    public static JSONObject toJson(ErrorResponse errorResponse) throws JsonProcessingException {
+        return new JSONObject()
+                .put("error", new JSONObject(new ObjectMapper().writeValueAsString(errorResponse)));
+    }
+}
