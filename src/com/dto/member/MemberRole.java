31d87a1cdf4c28a8bb3cada44c9db02e52079edb
pds0309 <ehd0309@gmail.com>
2022-02-24 10:14:27 +0900

    Feat: Member Role Enum 추가
    **MEMO**
    추후 권한 이미지 라던가 설명을 바꿀 수 있다.

---
+++ src/com/dto/member/MemberRole.java
@@ -0,0 +1,18 @@
+package com.dto.member;
+
+public enum MemberRole {
+    NORMAL("일반 사용자"),
+    MANAGER("매니저"),
+    BANNED("정지된 사용자"),
+    ADMIN("관리자");
+
+    private final String detail;
+
+    MemberRole(String detail) {
+        this.detail = detail;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+}
