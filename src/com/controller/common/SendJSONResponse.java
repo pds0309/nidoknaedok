40ad56c99f5ab0375e42e76e8860532469d42e95
pds0309 <ehd0309@gmail.com>
2022-02-25 01:38:38 +0900

    Feat: JSON 응답 결과 전달 정적 팩토리 메소드 추가
    정상결과 + 예외결과 메소드 오버로딩

---
+++ src/com/controller/common/SendJSONResponse.java
@@ -0,0 +1,41 @@
+package com.controller.common;
+
+import com.dto.common.ResponseDTO;
+import com.errors.ErrorCode;
+import com.errors.ErrorResponse;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * 결과를 JSON 데이터로 응답하는 메소드가 있다.
+ */
+public class SendJSONResponse {
+
+    private static final String CONTENT_TYPE = "application/json;charset=utf-8";
+
+    public static <T> void sendAsJson(HttpServletResponse response, T data, int status) throws IOException {
+        response.setContentType(CONTENT_TYPE);
+        response.setStatus(status);
+        PrintWriter out = response.getWriter();
+        out.print(ResponseDTO.toJson(status, data));
+        out.flush();
+    }
+
+    public static void sendAsJson(HttpServletResponse response, ErrorCode errorCode) throws IOException {
+        response.setContentType(CONTENT_TYPE);
+        response.setStatus(errorCode.getStatus());
+        PrintWriter out = response.getWriter();
+        out.print(ResponseDTO.toJson(ErrorResponse.of(errorCode)));
+        out.flush();
+    }
+
+    public static void sendAsJson(HttpServletResponse response, ErrorCode errorCode, String message) throws IOException {
+        response.setContentType(CONTENT_TYPE);
+        response.setStatus(errorCode.getStatus());
+        PrintWriter out = response.getWriter();
+        out.print(ResponseDTO.toJson(ErrorResponse.of(errorCode, message)));
+        out.flush();
+    }
+}
