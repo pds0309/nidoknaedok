a77117b9d8eccef41a47bbb92c363217ce5ecae2
pds0309 <ehd0309@gmail.com>
2022-02-25 15:49:46 +0900

    Feat: 이름, 이메일 유효성 검사 서블릿 추가

---
+++ src/com/controller/member/auth/CheckEmailServlet.java
@@ -0,0 +1,35 @@
+package com.controller.member.auth;
+
+import com.controller.common.SendJSONResponse;
+import com.service.member.MemberServiceImpl;
+import com.utils.RegexValidator;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet("/auth/email")
+public class CheckEmailServlet extends HttpServlet {
+    /**
+     * /auth/email
+     * GET
+     * 회원 가입에서 이메일을 검증하다
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("email");
+        RegexValidator.validEmail(email);
+
+        new MemberServiceImpl().validInputName(email);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("email", email);
+        SendJSONResponse.sendAsJson(response, map, response.getStatus());
+    }
+
+}
