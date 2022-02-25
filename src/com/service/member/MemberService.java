4ec07a6b40e94212455524abbc4ed50895dcb834
pds0309 <ehd0309@gmail.com>
2022-02-25 11:09:56 +0900

    Feat: 멤버 서비스 / 이메일, 이름으로 아이디 조회 로직 추가

--- src/com/service/member/MemberService.java
+++ src/com/service/member/MemberService.java
@@ -4,4 +4,8 @@ import com.dto.member.MemberDTO;
 
 public interface MemberService {
     int signUpMember(MemberDTO.SignUp member);
+
+    long existCheckByName(String name);
+
+    long existCheckByEmail(String email);
 }
--- src/com/service/member/MemberServiceImpl.java
+++ src/com/service/member/MemberServiceImpl.java
@@ -13,9 +13,34 @@ public class MemberServiceImpl implements MemberService {
         int status = 0;
         try {
             status = new MemberDAO().signUpMember(session, member);
+            session.commit();
         } finally {
             session.close();
         }
         return status;
+    }
+
+    @Override
+    public long existCheckByName(String name) {
+        SqlSession session = MySqlSessionFactory.getSession();
+        long result = 0;
+        try {
+            result = new MemberDAO().existCheckByName(session, name);
+        } finally {
+            session.close();
+        }
+        return result;
+    }
+
+    @Override
+    public long existCheckByEmail(String email) {
+        SqlSession session = MySqlSessionFactory.getSession();
+        long result = 0;
+        try {
+            result = new MemberDAO().existCheckByEmail(session, email);
+        } finally {
+            session.close();
+        }
+        return result;
     }
 }
