0840c8297b6e75190a03ea2da2b9366ad3ae1967
pds0309 <ehd0309@gmail.com>
2022-02-28 21:19:04 +0900

    Refactor: 멤버 가입 객체 빌더 패턴 적용

--- src/com/dto/member/MemberDTO.java
+++ src/com/dto/member/MemberDTO.java
@@ -2,6 +2,9 @@ package com.dto.member;
 
 import com.utils.RegexValidator;
 
+import javax.servlet.http.HttpSession;
+import java.io.Serializable;
+
 public class MemberDTO {
 
     private MemberDTO() {
@@ -18,20 +21,61 @@ public class MemberDTO {
         private String email;
         private String address;
         private String addressDetail;
-        private MemberRole authority;
+        private SocialType socialType;
+        private long socialId;
 
         public SignUp() {
             //
         }
 
-        public SignUp(String name, String password, String email, String address, String addressDetail) {
-            RegexValidator.validate(name, email, password);
-            this.name = name;
-            this.password = password;
-            this.email = email;
-            this.address = address;
-            this.addressDetail = addressDetail;
-            this.authority = MemberRole.NORMAL;
+        public SignUp(Builder builder) {
+            this.name = builder.name;
+            this.password = builder.password;
+            this.email = builder.email;
+            this.address = builder.address;
+            this.addressDetail = builder.addressDetail;
+            this.socialType = builder.socialType;
+            this.socialId = builder.socialId;
+        }
+
+        public static class Builder {
+            private final String name;
+            private final String password;
+            private String email;
+            private final String address;
+            private String addressDetail = "";
+            private SocialType socialType = SocialType.NORMAL;
+            private long socialId = -1;
+
+            public Builder(String name, String password, String address) {
+                this.name = name;
+                this.password = password;
+                this.address = address;
+            }
+
+            public Builder email(String value) {
+                this.email = value;
+                return this;
+            }
+
+            public Builder addressDetail(String value) {
+                this.addressDetail = value;
+                return this;
+            }
+
+            public Builder socialType(SocialType value) {
+                this.socialType = value;
+                return this;
+            }
+
+            public Builder socialId(long value) {
+                this.socialId = value;
+                return this;
+            }
+
+            public SignUp build() {
+                return new SignUp(this);
+            }
         }
 
         public String getName() {
@@ -54,16 +98,20 @@ public class MemberDTO {
             return addressDetail;
         }
 
-        public MemberRole getAuthority() {
-            return authority;
+        public SocialType getSocialType() {
+            return socialType;
+        }
+
+        public long getSocialId() {
+            return socialId;
         }
     }
 
     /**
      * 멤버 조회에 대한 모델
-     * 다양한 형태일 수 있다.
      */
-    public static class Info {
+    public static class Info implements Serializable {
+
         private long id;
         private String name;
         private String email;
@@ -74,9 +122,15 @@ public class MemberDTO {
         private int point;
         private String profileImage;
         private String introduction;
+        private SocialType socialType;
+        private long socialId;
 
         public Info() {
             //
+        }
+
+        public void addSession(HttpSession session) {
+            session.setAttribute("meminfo", this);
         }
 
         public long getId() {
@@ -117,6 +171,14 @@ public class MemberDTO {
 
         public String getIntroduction() {
             return introduction;
+        }
+
+        public SocialType getSocialType() {
+            return socialType;
+        }
+
+        public long getSocialId() {
+            return socialId;
         }
     }
 }
