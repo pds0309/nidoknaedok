9d182851709880b59504f0907d1d1534b5c79d14
pds0309 <ehd0309@gmail.com>
2022-03-03 15:23:05 +0900

    Feat: ajax 요청 컴포넌트 POST 방식 추가

--- web/components/FetchData.js
+++ web/components/FetchData.js
@@ -1,8 +1,21 @@
-export async function FetchData(url, method, type, data) {
+export async function FetchData(url, method, type, data, ...els) {
     if (method === 'GET') {
         const result = await fetch(url,
             {
                 method: method,
+            }
+        );
+        const json = await result.json();
+        return json;
+    }
+    if (method === 'POST') {
+        const result = await fetch(url,
+            {
+                method: method,
+                headers: {
+                    "Content-Type": type
+                },
+                body: data
             }
         );
         const json = await result.json();
