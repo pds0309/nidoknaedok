78d9207f410667f0086ae28982f47d55c258fac6
pds0309 <ehd0309@gmail.com>
2022-03-04 03:58:26 +0900

    Style: 로그아웃 경로 수정

--- src/com/controller/member/LogoutServlet.java
+++ src/com/controller/member/LogoutServlet.java
@@ -12,6 +12,6 @@ public class LogoutServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         request.getSession().invalidate();
-        response.sendRedirect(request.getContextPath() + "/home");
+        response.sendRedirect(request.getContextPath());
     }
 }
