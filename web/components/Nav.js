7f0ec46f9fc83435f966b4738ec343f474386bb5
pds0309 <ehd0309@gmail.com>
2022-03-04 04:03:12 +0900

    Feat: 화면 전환 컴포넌트 개선
    어떤 페이지들은 서버로 바로 이벤트가 흐를 수 있어야 한다.
    
    화면만 전환 가능한 모든 이벤트 발생에서는 세션의 유효함을 검증한다

--- web/components/Nav.js
+++ web/components/Nav.js
@@ -1,9 +1,27 @@
+import {CompareSession} from "./CompareSession.js";
+import {OpenDataSyncGET} from "./FetchData.js";
+
 export function Nav(render) {
+
+    const doNotPreventEventPages = [
+        {name: "logout", path: contextPath + "/members/logout"},
+    ]
+    const sessionTrace = CompareSession(Nav);
+
     document.addEventListener('click', ev => {
         if (!ev.target.matches('a')) {
             return;
         }
+
         const path = ev.target.getAttribute('href');
+        if (doNotPreventEventPages.find(page => page.path === path)) {
+            return;
+        }
+
+        const currentSession = JSON.parse(OpenDataSyncGET("session")).data.result;
+        if (sessionTrace.get !== currentSession) {
+            location.replace(contextPath + "/");
+        }
         ev.preventDefault();
         if (path !== window.location.pathname) {
             let previusHistoryURL;
