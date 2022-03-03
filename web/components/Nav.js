8dcf2736b0287b1e5709f4b13ffddac485d537c7
pds0309 <ehd0309@gmail.com>
2022-03-03 17:31:27 +0900

    Feat: 랜더링 컴포넌트 이벤트 및 history 관리 방법 수정
    기능 추가 상황에 맞게 수정되어야 할 것이다

--- web/components/Nav.js
+++ web/components/Nav.js
@@ -1,13 +1,19 @@
 export function Nav(render) {
-    navigation.addEventListener('click', ev => {
-        if (!ev.target.matches('#navigation > div > div > div > a')) {
+    document.addEventListener('click', ev => {
+        if (!ev.target.matches('a')) {
             return;
         }
         const path = ev.target.getAttribute('href');
-        console.log(path);
         ev.preventDefault();
         if (path !== window.location.pathname) {
-            window.history.pushState({}, null, path);
+            let previusHistoryURL;
+            if (window.history.state === null) {
+                previusHistoryURL = "/";
+            } else {
+                previusHistoryURL = window.history.state.current;
+            }
+            console.log("[Your Prev Page was]: " + previusHistoryURL);
+            window.history.pushState({current: path, prev: previusHistoryURL}, null, path);
             render(path);
         } else {
             console.log("[Prevent Re-render] same request!");
