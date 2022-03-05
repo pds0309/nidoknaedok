9df39aabb151559e686d9c696a3e5c14af19c8a0
pds0309 <ehd0309@gmail.com>
2022-03-05 21:09:46 +0900

    Feat: ErrorURL Enum 추가
    경로만으로 페이지 요청 예외를 동적으로 처리하려했으나 쉽지않았다.
    
    contextpath 때문인 것 같다.
    
    뷰 요청 URL Enum을 구성해 예외처리에 활용한다

---
+++ src/com/errors/ErrorURL.java
@@ -0,0 +1,37 @@
+package com.errors;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public enum ErrorURL {
+    U1("/login"),
+    U2("/join"),
+    U3("/kakaojoin"),
+    U4("/kakaointeg");
+
+    private final String url;
+
+    ErrorURL(String url) {
+        this.url = url;
+    }
+
+    private static Set<String> urlSet;
+
+    private static void initMapping() {
+        urlSet = new HashSet<>();
+        for (ErrorURL errorURL : values()) {
+            urlSet.add(errorURL.url);
+        }
+    }
+
+    public static Set<String> getURLSet() {
+        if (urlSet == null) {
+            initMapping();
+        }
+        return urlSet;
+    }
+
+    public static boolean exist(String url) {
+        return getURLSet().contains(url);
+    }
+}
