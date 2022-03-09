a7f9c006d7be08915af50fdf7646e15c6b72c3c8
pds0309 <ehd0309@gmail.com>
2022-03-09 10:28:53 +0900

    Chore: 탈퇴 관련 상수 추가

--- src/com/utils/Constants.java
+++ src/com/utils/Constants.java
@@ -9,5 +9,6 @@ public final class Constants {
     public static final String AUTH_COOKIE_NAME = "_katkauta";
     public static final int AUTH_COOKIE_TIMEOUT = 60 * 20;
     public static final int NORMAL_COOKIE_TIMEOUT = 60 * 60 * 6;
-
+    public static final String RESIGN_MEM_PROFILE = "https://yourdokzone.s3.ap-northeast-2.amazonaws.com/resign.JPG";
+    public static final int RESIGN_DATE = 3;
 }
